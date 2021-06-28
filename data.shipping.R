d802efc5577545b37facaae625247750bfd9e24c
Hubert Thieriot <hubert@energyandcleanair.org>
2021-06-28 15:00:03 +0800

    -added manual shipping

---
+++ data.shipping.R
@@ -0,0 +1,42 @@
+
+#' Build simplified road network, keeping only required levels, and adding the bps id to it
+#' so that it can be merged with emission data
+#'
+#' @return
+#' @export
+#'
+#' @examples
+data.build_shipping_support <- function(){
+
+  library(tidygeocoder)
+
+  # Use manual data
+  harbours <- c(
+    "Pelabuhan Tanjung Priok",
+    "Bakauheni Port, Lampung",
+    "Merak, Cilegon, Banten"
+    )
+
+  locs <- tidygeocoder::geocode(.tbl=tibble(address=harbours), address="address")
+
+  # Get bps map
+  g <- data.bps_map()
+
+  locs.sf <- locs %>%
+    sf::st_as_sf(coords=c("long","lat"), crs=4326) %>%
+    sf::st_join(g, left=F) %>%
+    dplyr::select(airport=address, id, geometry) %>%
+    mutate(weight=1)
+
+  locs.sf %>% sf::write_sf("data/shipping/harbours.shp")
+
+  return(locs.sf)
+}
+
+data.shipping_support <- function(){
+  sf::read_sf("data/shipping/harbours.shp")
+}
+
+data.shipping_emission <- function(){
+  data.sheet_to_emissions(sheet_name="Shipping")
+}
