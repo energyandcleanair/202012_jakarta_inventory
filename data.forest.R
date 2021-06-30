b6767b894f275126aa66d244efcff1df00835da4
Hubert Thieriot <hubert.thieriot@gmail.com>
2021-06-30 06:57:43 +0000

    -upscaled forest fires to provincial level

--- data.forest.R
+++ data.forest.R
@@ -1,4 +1,12 @@
-#' Build support required for Commercial & residential
+# Forest Fires
+# Hubert: The original data used in the report is at a provincial level: http://sipongi.menlhk.go.id/hotspot/luas_kebakaran
+# Prof. Didin probably scaled it down to kota and kabupaten based on some area coverage.
+# However, when looking at VIIRS data (on which is based original data), not all kota/kabupaten have detected fires in 2019.
+# Solution: we bring back the data to the original regional scale, and attribute emissions down to each pixel based on detected FRPs.
+
+
+
+#' Build support at province level
 #'
 #' @return
 #' @export
@@ -35,8 +43,8 @@ data.build_forest_support <- function(){
                        extent.sp=extent.sp)
 
   fires_w_id <- fires %>%
-    sf::st_join(intersection %>% select(id)) %>%
-    rename(weight=frp)
+    select(weight=frp) %>%
+    sf::st_join(intersection %>% select(id=province))
 
   fires_w_id$acq_date <- NULL
   # sf::write tooo slow
@@ -58,5 +66,13 @@ data.forest_support <- function(){
 #'
 #' @examples
 data.forest_emission <- function(){
-  data.sheet_to_emissions(sheet_name="Forest Fire")
+  e <- data.sheet_to_emissions(sheet_name="Forest Fire")
+  g <- data.bps_map() %>%
+    sf::st_make_valid()
+
+  # Using province as id
+  e %>%
+    left_join(g %>% select(id, province)) %>%
+    group_by(id=province, poll, unit, year) %>%
+    summarise_at("emission", sum, na.rm=T)
 }
